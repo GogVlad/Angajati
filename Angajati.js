0d7eb8244c757e94f7f6224a7081677d64e69fca
AzureAD\Vlad-TeodorGogu <vlad.gogu@principal33.com>
2021-04-26 12:30:51 +0300

    Implemented delete from table

--- Angajati.js
+++ Angajati.js
@@ -12,8 +12,38 @@ function onClickSubmit() {
                           <td>${sex}</td>
                           <td>${email}</td>
                           <td>${dataNastere}</td>
+                          <td><input type="button" value="Delete" onclick="deleteRow(this)"></td>
                       </tr>
                           `;
 
   document.getElementById("angajatiT").innerHTML += tableContent;
 }
+
+/*function onClickDelete() {
+    try {
+    var table = document.getElementById("angajatiT");
+    var rowCount = table.rows.length;
+
+    for(var i=0; i<rowCount; i++) {
+        var row = table.rows[i];
+        var chkbox = row.cells[5].childNodes[5];     TypeError: Cannot read property 'childNodes' of undefined
+        if(null != chkbox && true == chkbox.checked) {
+            if(rowCount <= 1) {
+                alert("Cannot delete all the rows.");
+                break;
+            }
+            table.deleteRow(i);
+            rowCount--;
+            i--;
+        }
+
+
+    }
+    }catch(e) {
+        alert(e);
+    }
+}*/
+function deleteRow(r) {
+    var i = r.parentNode.parentNode.rowIndex;
+    document.getElementById("angajatiT").deleteRow(i);
+  }
