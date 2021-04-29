df8ab05af842d775b8739e01a82e4978c2a148d5
AzureAD\Vlad-TeodorGogu <vlad.gogu@principal33.com>
2021-04-29 13:04:53 +0300

    Changed js code so it fetches the employees from the database and it can add employees to the database

--- Angajati.js
+++ Angajati.js
@@ -1,28 +1,86 @@
-function onClickSubmit() {
-  let nume = document.getElementById("numeAngajat").value;
-  let lastName = document.getElementById("prenumeAngajat").value;
-  let sex = document.getElementById("emailAngajat").value;
-  let email = document.getElementById("genAngajat").value;
-  let dataNastere = document.getElementById("dataN").value;
+var employeesList=[];
 
-  let tableContent = `
-                      <tr>
-                          <td>${nume}</td>
-                          <td>${lastName}</td>
-                          <td>${sex}</td>
-                          <td>${email}</td>
-                          <td>${dataNastere}</td>
-                          <td><input type="button" value="Delete" onclick="deleteRow(this)"></td>
-                      </tr>
-                          `;
+jQuery(document).ready(function ($) {
+    $.ajax({
+        method: "GET",
+        url: 'https://localhost:5001/employee/Employee',
+        success: function (data) {
+            employeesList = data;
+            loadEmployees(employeesList);
+        },
+        error: function (data) {
+        alert(`Failed to load employees list.`);
+        },
+    });
 
-  document.getElementById("angajatiT").innerHTML += tableContent;
+});
+   
+var monthNames = ["January", "February", "March", "April", "May","June","July", "August", "September", "October", "November","December"];
+
+function loadEmployees(employeesList){
+    for (index = 0; index < employeesList.length; index++)
+    { 
+        appendRow(employeesList[index])
+    }
+}
+function validateInput(newEmployee){
+        
+    if (!newEmployee.FirstName || !newEmployee.LastName || !newEmployee.Email) {
+        return false;
+    }
+    return true;
+}
+        
+function addEmployee() {
+
+    var newEmployee = new Object();
+    newEmployee.Id = 0;
+    newEmployee.FirstName = document.getElementById('prenumeAngajat').value;
+    newEmployee.LastName = document.getElementById('numeAngajat').value;
+    newEmployee.Email = document.getElementById('emailAngajat').value;
+    if (!validateInput(newEmployee)) {
+        alert("You need to fill all the information!");
+        return;
+    }
+    newEmployee.Gender = document.getElementById('genAngajat').value;
+    
+    newEmployee.Birthdate = document.getElementById('dataN').value;
+
+    $.ajax({
+        method: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(newEmployee),
+        url: 'https://localhost:5001/employee/Employee',
+        success: function (data) {
+            appendRow(data)
+        },
+        error: function (data) {
+        alert(`Failed to load employees list.`);
+        },
+    });
+    
 }
 
-function deleteRow(r) {
-    var i = r.parentNode.parentNode.rowIndex;
-    document.getElementById("angajatiT").deleteRow(i);
-  }
+function appendRow(employee){
+    let employeesTable = document.querySelector("table");
+    var rowId = employeesTable.getElementsByTagName("tr").length;
+    let newRow = ` <tr>
+                        <td id="firstName">${employee.firstName}</td>
+                        <td id="lastName">${employee.lastName}</td>
+                        <td id="email">${employee.email}</td>
+                        <td id="gender">${employee.gender}</td>
+                        <td id="birthdate">${employee.birthdate}</td>
+                        <td><input type="button" value="Delete" onclick="deleteEmployee(this)"></td>
+                    </tr>`;
+                
+    employeesTable.innerHTML += newRow;
+
+}
+
+function deleteEmployee(elem) {
+    var row = elem.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
 
 function searchEmployee() {
     // Declare variables
